2b8cf5986884126e0d15f6500cb510b6704fa7f3
9 <pray4spam@googlemail.com>
2022-02-11 09:57:24 +0000

    # in log function changed timestamp behaviour

--- KFS/log.py
+++ KFS/log.py
@@ -3,12 +3,19 @@ import math
 import os
 
 
-line_last_len=0 #Zeile letzte Länge um evt. zu überschreiben
+line_last_len=0                 #Zeile letzte Länge um evt. zu überschreiben
+timestamp_old=""                #Zeitstempel zuletzt ausgegeben
+timestamp_old_line_previous=""  #Zeitstempel zuletzt ausgegeben auf Zeile davor
 def write(text: str, append_to_line_current: bool=False, UNIX_time: bool=False) -> None:
     global line_last_len
+    global timestamp_old
+    global timestamp_old_line_previous
     newline_replacement="\n"        #womit soll Zeilenumbruch ersetzt werden? (Zeilenumbruch + Einrückung)
     overwrite_line_current=False    #Zeile letzte überschreiben?
-    timestamp=""                    #Zeitstempel vor Logeintrag
+    timestamp=""                    #Zeitstempel vor Logeintrag, entweder richtiger Zeitstempel oder Leerzeichen wenn gleich wie davor
+    timestamp_in_console=""         #Zeitstempel in Konsolenlog hinzufügen ("none", "just spaces", "full")
+    timestamp_in_file=""            #Zeitstemp in Logdatei hinzufügen ("none", "just spaces", "full")
+    timestamp_new=""                #Zeitstempel aktuell, nur bei Modus "full" verwenden
 
 
     try:
@@ -21,7 +28,12 @@ def write(text: str, append_to_line_current: bool=False, UNIX_time: bool=False) 
         raise TypeError("Error in KFS::log::write(...): Type of \"UNIX_time\" must be bool.")
 
 
-    DT_now=dt.datetime.now(dt.timezone.utc) #Zeitpunkt aktuell
+    DT_now=dt.datetime.now(dt.timezone.utc)                                         #Zeitpunkt aktuell
+    if UNIX_time==False:                                                            #wenn nicht im Unix-Format:
+        timestamp_new=f"[{DT_now.strftime('%Y-%m-%dT%H:%M:%SZ')}]"                  #Zeitstempel neu nach ISO8601
+    else:
+        timestamp_new=f"[{math.floor(DT_now.timestamp()):,.0f}]".replace(",", ".")  #Zeitstempel neu im Unixformat
+    
     os.makedirs("./Log/", exist_ok=True)    #Log-Ordner erstellen
 
     if text[0:1]=="\r":             #wenn Zeichen [0] Carriage Return: Zeile letzte überschreiben, Inhalte vorher löschen
@@ -31,29 +43,60 @@ def write(text: str, append_to_line_current: bool=False, UNIX_time: bool=False) 
             print("                                                                                                    ", end="", flush=True)
         text=text[1:]               #\r entfernen
 
-    if overwrite_line_current==False and append_to_line_current==False: #wenn Zeile aktuell nicht überschreiben und nicht an Zeile aktuell angehangen werden soll:
-        print("\n", end="", flush=True)                                 #in Konsole Zeilenumbruch
-    
-    if append_to_line_current==False:                                               #wenn nicht einfach an Zeile angehangen werden soll:
-        with open(f"./Log/{DT_now.strftime('%Y-%m-%d Log.txt')}", "at") as log_file:
-            log_file.write(f"\n")                                                   #in Datei Zeilenumbruch, in Datei kein Zeile überschreiben gewollt
-        
-        if UNIX_time==False:                                                        #wenn nicht im Unix-Format:
-            timestamp=f"[{DT_now.strftime('%Y-%m-%dT%H:%M:%SZ')}]"                  #Zeitstempel nach ISO8601
-        else:
-            timestamp=f"[{math.floor(DT_now.timestamp()):,.0f}]".replace(",", ".")  #Zeitstempel im Unixformat
-        text=f"{timestamp} {text}"                                                  #Zeitstempel hinzufügen
-    
-    for i in range(len(timestamp)+1):               #Einrückungsbreite
+    for i in range(len(timestamp_new)+1):           #Einrückungsbreite
         newline_replacement+=" " 
-    text=text.replace("\n", newline_replacement)    #Text einrücken
+    text=text.replace("\n", newline_replacement)    #Text nach Zeilenumbruch einrücken
+
+    line_last_len=len(timestamp_new)+1+len(text)    #Zeilenlänge merken um nächstes Mal evt. zu überschreiben
     
-    line_last_len=len(text) #Zeilenlänge merken um nächstes Mal evt. zu überschreiben
+    if overwrite_line_current==False and append_to_line_current==False and timestamp_old!="":   #wenn Zeile neu:
+        timestamp_old_line_previous=timestamp_old                                               #Zeitstempel aktualisieren auf Zeile zuvor
+    if timestamp_old_line_previous!=timestamp_new:  #wenn Zeitstempel anders als auf Zeile zuvor ausgegeben:
+        timestamp_in_console="full"                 #in Konsole Zeitstempel anzeigen
+    else:                                           #wenn Zeitstempel gleich wie in Zeile zuvor ausgegeben:
+        timestamp_in_console="just spaces"          #kein Zeitstempel, nur Einrückung
+    if timestamp_old!=timestamp_new:        #wenn Zeitstempel anders als zuletzt ausgegeben:
+        timestamp_in_file="full"            #in Logdatei Zeitstempel schreiben
+    else:                                   #wenn Zeitstempel gleich wie zuletzt ausgegeben:
+        timestamp_in_file="just spaces"     #kein Zeitstempel, nur Einrückung
+
+    if overwrite_line_current==True:    #wenn Zeile aktuell überschreiben:
+        print("\r", end="")             #in Konsole Carriage Return
+        with open(f"./Log/{DT_now.strftime('%Y-%m-%d Log.txt')}", "at") as log_file:    
+            log_file.write(f"\n")       #in Datei aber Zeilenumbruch
+        timestamp_old=timestamp_new     #Zeitstempel zuletzt ausgegeben aktualisieren
+    elif append_to_line_current==True:  #wenn in Zeile aktuell dranhängen:
+        timestamp_in_console="none"     #Standardzeitstempeleinstellungen ignorieren, immer kein Zeitstempel, auch keine Einrückung
+        timestamp_in_file="none"
+    else:                           #wenn Zeile neu:
+        print("\n", end="")         #in Konsole Zeilenumbruch
+        with open(f"./Log/{DT_now.strftime('%Y-%m-%d Log.txt')}", "at") as log_file:
+            log_file.write(f"\n")   #in Datei Zeilenumbruch
+        timestamp_old=timestamp_new                 #Zeitstempel zuletzt ausgegeben aktualisieren
+   
+
+    if timestamp_in_console=="full":            #wenn Zeitstempel gewünscht:
+        timestamp=f"{timestamp_new} "
+    elif timestamp_in_console=="just spaces":   #wenn Einrückung gewünscht:
+        for i in range(len(timestamp_old)+1):   #Einrückungsbreite
+            timestamp+=" "                      #kein Zeitstempel ausgeben, nur Einrückung
+    elif timestamp_in_console=="none":          #wenn nix gewünscht:
+        timestamp=""
+    else:
+        raise RuntimeError(f"Error in KFS::log::write(...): timestamp_in_console has invalid value which should not occur (\"{timestamp_in_console}\").")
+    print(f"{timestamp}{text}", end="", flush=True)
+    timestamp=""    #vor Zeitstempelbestimmung für Datei Zeitstempel zurücksetzen
     
-    
-    if overwrite_line_current==True:        #wenn Zeile überschrieben werden soll: in Konsole Carriage Return
-        print("\r", end="")
-    print(f"{text}", end="", flush=True)    #in Konsole Text drucken
+    if timestamp_in_file=="full":               #wenn Zeitstempel gewünscht:
+        timestamp=f"{timestamp_new} "
+    elif timestamp_in_file=="just spaces":      #wenn Einrückung gewünscht:
+        for i in range(len(timestamp_old)+1):   #Einrückungsbreite
+            timestamp+=" "                      #kein Zeitstempel ausgeben, nur Einrückung
+    elif timestamp_in_file=="none":             #wenn nix gewünscht:
+        timestamp=""
+    else:
+        raise RuntimeError(f"Error in KFS::log::write(...): timestamp_in_file has invalid value which should not occur (\"{timestamp_in_file}\").")
     with open(f"./Log/{DT_now.strftime('%Y-%m-%d Log.txt')}", "at") as log_file:
-        log_file.write(f"{text}")           #in Datei Text schreiben
+        log_file.write(f"{timestamp}{text}")
+
     return
