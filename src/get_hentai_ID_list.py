a2b9e8cc80e9dea165ad30dc390a7abc855afd87
9FS <pray4spam@googlemail.com>
2023-09-26 20:42:15 +0000

    unified hentai_ID_list conversion from list[str] to list[int] and data cleanup in own function

--- src/get_hentai_ID_list.py
+++ src/get_hentai_ID_list.py
@@ -12,30 +12,49 @@ def get_hentai_ID_list() -> list[int]:
     """
 
     file_tried: bool=False          # tried to load from file?
-    hentai_ID_list: list[int]=[]    # hentai ID list
-    hentai_ID_list_str: list[str]
+    hentai_ID_list: list[int]=[]    # hentai ID list to download
 
     
     while True:
-        if os.path.isfile("./downloadme.txt")==True and file_tried==False:  # if ID list in file and not tried to load from file yet: load from file, only try once
+        if os.path.isfile("./downloadme.txt")==True and file_tried==False:                                      # if ID list in file and not tried to load from file yet: load from file, only try once
             file_tried=True
             with open("downloadme.txt", "rt") as downloadme_file:
-                hentai_ID_list_str=downloadme_file.readlines()              # read all lines from file
-        else:                                                               # if ID list file not available: ask user for input
+                hentai_ID_list=_convert_hentai_ID_list_str_to_hentai_ID_list_int(downloadme_file.readlines())   # read all hentai ID from file, list[int] -> list[str]
+        else:                                                                                                   # if ID list file not available: ask user for input
             logging.info("Enter the holy numbers: ")
-            hentai_ID_list_str=input().split(" ")                           # user input seperated at whitespace
+            hentai_ID_list=_convert_hentai_ID_list_str_to_hentai_ID_list_int(input().split(" "))                # user input seperated at whitespace, list[int] -> list[str]
         
-        hentai_ID_list_str=[hentai_ID for hentai_ID in hentai_ID_list_str if len(hentai_ID)!=0] # remove empty inputs
-        if len(hentai_ID_list_str)==0:                                                          # if file or user input empty: retry
+        if len(hentai_ID_list)==0:  # if file or user input empty: retry
             continue
 
-        for hentai_ID in hentai_ID_list_str:    # convert all hentai ID to int
-            try:
-                hentai_ID_list.append(int(hentai_ID))
-            except ValueError:                  # if input invalid: discard whole input, ask user (again)
-                logging.error(f"Converting input \"{hentai_ID}\" to int failed.")
-                break
-        else:                                   # if all ID converted without failure: break out of while, return desired ID
-            break
+        break
+
+    return hentai_ID_list
+
+
+def _convert_hentai_ID_list_str_to_hentai_ID_list_int(hentai_ID_list_str: list[str]) -> list[int]:
+    """
+    Converts list of hentai ID from list[str] to list[int] cleans up entries.
+
+    Arguments:
+    - hentai_ID_list_str: list of hentai ID in str to convert
+
+    Returns:
+    - hentai_ID_list: list of hentai ID in int
+    """
+
+    hentai_ID_list: list[int]=[]    # list of hentai ID in int
+
+
+    hentai_ID_list_str=[hentai_ID for hentai_ID in hentai_ID_list_str if len(hentai_ID)!=0] # throw out emtpy entries
+    hentai_ID_list_str=list(dict.fromkeys(hentai_ID_list_str))                              # remove duplicates
+
+    for hentai_ID in hentai_ID_list_str:                                                    # list[str] -> list[int]
+        try:
+            hentai_ID_list.append(int(hentai_ID))
+        except ValueError:                                                                  # if input invalid: discard that, keep rest
+            logging.error(f"Converting input \"{hentai_ID}\" to int failed. Skipping ID.")
+            
+    hentai_ID_list=sorted(hentai_ID_list)                                                   # sort numerically
 
     return hentai_ID_list
