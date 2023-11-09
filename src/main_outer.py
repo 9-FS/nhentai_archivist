578807eac156372a58d8fa7e65f3ce05391831aa
9FS <pray4spam@googlemail.com>
2023-11-09 13:47:47 +0000

    fixed multiprocessing not working on windows executables

--- src/main_outer.py
+++ src/main_outer.py
@@ -1,11 +1,13 @@
 # Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+from KFSlog import KFSlog
+import logging
+import multiprocessing
+import traceback
+from main import main
+
+
 if __name__=="__main__":
-    from KFSlog import KFSlog
-    import logging
-    import traceback
-    from main import main
-
-
+    multiprocessing.freeze_support()    # for multiprocessing to work on windows executables
     KFSlog.setup_logging("", logging.INFO)
     #KFSlog.setup_logging("", logging.DEBUG, filepath_format="./log/%Y-%m-%dT%H_%M.log", rotate_filepath_when="M")
 
