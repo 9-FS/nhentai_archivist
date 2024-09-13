3f1899fcdecdb758c29de98640006e07c605ec7d
9FS <pray4spam@googlemail.com>
2024-09-13 20:11:05 +0000

    dropped as many variables as possible between runs

--- src/main_inner.rs
+++ src/main_inner.rs
@@ -8,9 +8,9 @@ use crate::hentai::*;
 
 pub async fn main_inner(config: Config) -> Result<(), Error>
 {
-    const NHENTAI_HENTAI_SEARCH_URL: &str="https://nhentai.net/api/gallery/"; // nhentai search by id api url
-    const NHENTAI_TAG_SEARCH_URL: &str="https://nhentai.net/api/galleries/search"; // nhentai search by tag api url
-    let mut db: sqlx::sqlite::SqlitePool; // database containing all metadata from nhentai.net api
+    const HTTP_TIMEOUT: u64 = 30; // connection timeout
+    const NHENTAI_HENTAI_SEARCH_URL: &str = "https://nhentai.net/api/gallery/"; // nhentai search by id api url
+    const NHENTAI_TAG_SEARCH_URL: &str = "https://nhentai.net/api/galleries/search"; // nhentai search by tag api url
     let f0 = scaler::Formatter::new()
         .set_scaling(scaler::Scaling::None)
         .set_rounding(scaler::Rounding::Magnitude(0)); // formatter
@@ -18,94 +18,89 @@ pub async fn main_inner(config: Config) -> Result<(), Error>
         .set_scaling(scaler::Scaling::None)
         .set_rounding(scaler::Rounding::Magnitude(-2)); // formatter
     let f4 = scaler::Formatter::new(); // formatter
-    let mut hentai_id_list: Vec<u32>; // list of hentai id to download
-    let http_client: reqwest::Client; // http client
-    let timeout: std::time::Duration = std::time::Duration::from_secs(30); // connection timeout
-
-
-    {
-        let mut headers: reqwest::header::HeaderMap = reqwest::header::HeaderMap::new(); // headers
-        match reqwest::header::HeaderValue::from_str(&config.USER_AGENT)
-        {
-            Ok(o) => _ = headers.insert(reqwest::header::USER_AGENT, o),
-            Err(e) => log::warn!("Adding user agent to HTTP client headers failed with: {e}\nUsing empty user agent instead."),
-        }
-        match reqwest::header::HeaderValue::from_str(format!("cf_clearance={}; csrftoken={}", config.CF_CLEARANCE, config.CSRFTOKEN).as_str())
-        {
-            Ok(o) => _ = headers.insert(reqwest::header::COOKIE, o),
-            Err(e) => log::warn!("Adding cookies \"cf_clearance\" and \"csrftoken\" to HTTP client headers failed with: {e}\nUsing no cookies instead."),
-        }
-        match reqwest::Client::builder()  // create http client
-            .connect_timeout(timeout)
-            .cookie_store(true) // enable cookies
-            .default_headers(headers)
-            .read_timeout(timeout)
-            .build()
-        {
-            Ok(o) => http_client = o,
-            Err(e) => return Err(Error::ReqwestClientBuilder {source: e}),
-        }
-        let r: reqwest::Response = http_client.get(NHENTAI_TAG_SEARCH_URL).query(&[("query", "language:english"), ("page", "1")]).send().await?; // send test request
-        if r.status() != reqwest::StatusCode::OK // if status is not ok: something went wrong
-        {
-            return Err(Error::ReqwestStatus {url: r.url().to_string(), status: r.status()});
-        }
-    }
 
 
     loop // keep running for server mode
     {
-        db = connect_to_db(&config.DATABASE_URL).await?; // connect to database
-        hentai_id_list = get_hentai_id_list
-        (
-            config.DOWNLOADME_FILEPATH.as_str(),
-            &http_client,
-            NHENTAI_TAG_SEARCH_URL,
-            &config.NHENTAI_TAG,
-            &db,
-        ).await;
-
-
-        for (i, hentai_id) in hentai_id_list.iter().enumerate()
         {
-            log::info!("--------------------------------------------------");
-            log::info!("{} / {} ({}) | hentai {hentai_id}", f0.format((i+1) as f64), f0.format(hentai_id_list.len() as f64), fm2.format((i+1) as f64 / hentai_id_list.len() as f64));
-            let hentai: Hentai; // hentai to download
+            let db: sqlx::sqlite::SqlitePool; // database containing all metadata from nhentai.net api
+            let hentai_id_list: Vec<u32>; // list of hentai id to download
+            let http_client: reqwest::Client; // http client
 
 
-            if (i + 1).rem_euclid(1000) == 0 // reconnect to database every 1000 downloads to free up resources
+            let mut headers: reqwest::header::HeaderMap = reqwest::header::HeaderMap::new(); // headers
+            match reqwest::header::HeaderValue::from_str(&config.USER_AGENT)
             {
-                db.close().await; // close database connection
-                log::info!("Disconnected from database at \"{}\".", config.DATABASE_URL);
-                db = connect_to_db(&config.DATABASE_URL).await?; // reconnect to database
+                Ok(o) => _ = headers.insert(reqwest::header::USER_AGENT, o),
+                Err(e) => log::warn!("Adding user agent to HTTP client headers failed with: {e}\nUsing empty user agent instead."),
+            }
+            match reqwest::header::HeaderValue::from_str(format!("cf_clearance={}; csrftoken={}", config.CF_CLEARANCE, config.CSRFTOKEN).as_str())
+            {
+                Ok(o) => _ = headers.insert(reqwest::header::COOKIE, o),
+                Err(e) => log::warn!("Adding cookies \"cf_clearance\" and \"csrftoken\" to HTTP client headers failed with: {e}\nUsing no cookies instead."),
+            }
+            match reqwest::Client::builder()  // create http client
+                .connect_timeout(std::time::Duration::from_secs(HTTP_TIMEOUT))
+                .cookie_store(true) // enable cookies
+                .default_headers(headers)
+                .read_timeout(std::time::Duration::from_secs(HTTP_TIMEOUT))
+                .build()
+            {
+                Ok(o) => http_client = o,
+                Err(e) => return Err(Error::ReqwestClientBuilder {source: e}),
+            }
+            let r: reqwest::Response = http_client.get(NHENTAI_TAG_SEARCH_URL).query(&[("query", "language:english"), ("page", "1")]).send().await?; // send test request
+            if r.status() != reqwest::StatusCode::OK && r.status() != reqwest::StatusCode::TOO_MANY_REQUESTS // if status is not ok and not too many requests: something went wrong, abort
+            {
+                return Err(Error::ReqwestStatus {url: r.url().to_string(), status: r.status()});
             }
 
-            match Hentai::new(*hentai_id, &db, &http_client, NHENTAI_HENTAI_SEARCH_URL, &config.LIBRARY_PATH, config.LIBRARY_SPLIT).await
+            db = connect_to_db(&config.DATABASE_URL).await?; // connect to database
+            hentai_id_list = get_hentai_id_list
+            (
+                config.DOWNLOADME_FILEPATH.as_str(),
+                &http_client,
+                NHENTAI_TAG_SEARCH_URL,
+                &config.NHENTAI_TAG,
+                &db,
+            ).await;
+
+
+            for (i, hentai_id) in hentai_id_list.iter().enumerate()
             {
-                Ok(o) => hentai = o, // hentai created successfully
-                Err(e) => // hentai creation failed
+                log::info!("--------------------------------------------------");
+                log::info!("{} / {} ({}) | hentai {hentai_id}", f0.format((i+1) as f64), f0.format(hentai_id_list.len() as f64), fm2.format((i+1) as f64 / hentai_id_list.len() as f64));
+                let hentai: Hentai; // hentai to download
+
+
+                match Hentai::new(*hentai_id, &db, &http_client, NHENTAI_HENTAI_SEARCH_URL, &config.LIBRARY_PATH, config.LIBRARY_SPLIT).await
                 {
-                    log::error!("{e}");
-                    continue; // skip download
+                    Ok(o) => hentai = o, // hentai created successfully
+                    Err(e) => // hentai creation failed
+                    {
+                        log::error!("{e}");
+                        continue; // skip download
+                    }
+                }
+
+                if let Err(e) = hentai.download(&http_client).await
+                {
+                    log::error!{"{e}"};
                 }
             }
+            log::info!("--------------------------------------------------");
 
-            if let Err(e) = hentai.download(&http_client).await
+
+            db.close().await; // close database connection
+            log::info!("Disconnected from database at \"{}\".", config.DATABASE_URL);
+
+            if config.NHENTAI_TAG.is_none() {break;} // if tag not set: client mode, exit
+
+            if let Err(e) = tokio::fs::remove_file(&config.DOWNLOADME_FILEPATH).await // server mode cleanup, delete downloadme
             {
-                log::error!{"{e}"};
+                log::warn!("Deleting \"{}\" failed with: {e}", config.DOWNLOADME_FILEPATH);
             }
-        }
-        log::info!("--------------------------------------------------");
-
-
-        if config.NHENTAI_TAG.is_none() {break;} // if tag not set: client mode, exit
-
-        if let Err(e) = tokio::fs::remove_file(&config.DOWNLOADME_FILEPATH).await // server mode cleanup, delete downloadme
-        {
-            log::warn!("Deleting \"{}\" failed with: {e}", config.DOWNLOADME_FILEPATH);
-        }
-        db.close().await; // close database connection
-        log::info!("Disconnected from database at \"{}\".", config.DATABASE_URL);
+        } // free as much memory as possible
 
         log::info!("Sleeping for {}s...", f4.format(config.SLEEP_INTERVAL.unwrap_or_default() as f64));
         tokio::time::sleep(std::time::Duration::from_secs(config.SLEEP_INTERVAL.unwrap_or_default())).await; // if in server mode: sleep for interval until next check
@@ -114,5 +109,3 @@ pub async fn main_inner(config: Config) -> Result<(), Error>
 
     return Ok(());
 }
-
-// https://nhentai.net/api/galleries/search?query=language%3Aenglish
